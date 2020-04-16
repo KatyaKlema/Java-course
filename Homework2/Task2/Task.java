be78491707e1ff8229639fe64cb91050af9848ab
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-04-16 21:10:22 +0300

    Update Task.java

--- Homework2/Task2/Task.java
+++ Homework2/Task2/Task.java
@@ -3,21 +3,39 @@ import java.util.concurrent.Callable;
 public class Task<T> {
     private T currentResult = null;
     private Callable<? extends T> callable;
+    private Boolean isCalled;
     public Task(Callable<? extends T> callable){
         this.callable = callable;
+        this.isCalled = false;
     }
     public T get() throws Exception {
         if(currentResult != null){
             return currentResult;
         }
-        synchronized (callable){
+        if(isCalled){
+            synchronized (this){
+                while(currentResult == null){
+                    try{
+                        this.wait();
+                    }
+                    catch(RuntimeException runtimeException){
+                        throw new CallException();
+                    }
+                }
+            }
+        }
+        else{
+            isCalled = true;
             try {
                 currentResult = callable.call();
+                synchronized(this){
+                    this.notifyAll();   
+                }
+                return currentResult; 
             }
             catch(RuntimeException runtimeException){
                 throw new CallException();
             }
-            return currentResult;
         }
     }
 }
