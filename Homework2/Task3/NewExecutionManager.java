23c5f8729e9a50de1143d2aa0d212259240ec0de
Kate Klem <43951601+KatyaKlema@users.noreply.github.com>
2020-05-21 13:03:47 +0300

    Update NewExecutionManager.java

--- Homework2/Task3/NewExecutionManager.java
+++ Homework2/Task3/NewExecutionManager.java
@@ -54,31 +54,4 @@ class NewExecutionManage implements ExecutionManage {
         main.start();
         return new NewContext();
     }
-
-    public class NewContext implements Context {
-        @Override
-        public int getCompleteTaskCount() {
-            return finishedTasks.get();
-        }
-
-        @Override
-        public int getFailedTaskCount() {
-            return faildTasks.get();
-        }
-
-        @Override
-        public int getInterruptedTaskCount() {
-            return interruptedTasks.get();
-        }
-
-        @Override
-        public void interrupt() {
-            isInterrupted = true;
-        }
-
-        @Override
-        public boolean isFinished() {
-            return isFinished;
-        }
-    }
 }
